bf915dc8a6cd8c4ba75b3abf42ca08b174b13878
victorocna <victorocnarescu@gmail.com>
2020-03-08 23:23:31 +0200

    initial commit

---
+++ postmessage.js
@@ -0,0 +1,57 @@
+/**
+ * @see https://medium.com/better-programming/how-to-automatically-resize-an-iframe-7be6bfbb1214
+ */
+let height;
+const sendPostMessage = () => {
+  if (height !== document.getElementsByTagName('body')[0].offsetHeight) {
+    height = document.getElementsByTagName('body')[0].offsetHeight;
+
+    // FIXME: remove logs
+    console.log('new height for iframe', height);
+
+    window.parent.postMessage({
+      frameHeight: height
+    }, '*');
+  }
+};
+
+const listenForTextareaResize = () => {
+  const textareas = document.getElementsByTagName('textarea')
+  for (let i = 0; i < textareas.length; i++) {
+    const ta = textareas[i]
+
+    var _width = ta.clientWidth;
+    var _height = ta.clientHeight;
+    ta.addEventListener('mousemove', function () {
+      if (ta.clientWidth != _width || ta.clientHeight != _height) {
+        sendPostMessage();
+      }
+      _width = ta.clientWidth;
+      _height = ta.clientHeight;
+    });
+  }
+}
+
+const listenForDomMutation = () => {
+  MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
+
+  var observer = new MutationObserver(function () {
+    sendPostMessage();
+  });
+
+  // define what element should be observed by the observer
+  // and what types of mutations trigger the callback
+  observer.observe(document, {
+    subtree: true,
+    attributes: true
+  });
+}
+
+window.onresize = () => {
+  sendPostMessage();
+}
+window.onload = () => {
+  sendPostMessage();
+  listenForTextareaResize();
+  listenForDomMutation();
+}
